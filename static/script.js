8728ca003016c045fdca0f1f9215a99eccc9c5db
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-05-12 13:13:28 -0400

    Some updates and styling.

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+/* 
 window.addEventListener('DOMContentLoaded', (event) => {
     const courseDetails = document.getElementsByClassName('course-details');
     for (let i = 0; i < courseDetails.length; i++) {
@@ -5,6 +6,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
+ 
 function toggleDetails(prefix) {
     const courseDetails = document.getElementById(prefix);
     if (courseDetails.style.display === 'none') {
@@ -12,4 +14,19 @@ function toggleDetails(prefix) {
     } else {
         courseDetails.style.display = 'none';
     }
+}
+*/
+
+function toggleDetails(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none") {
+        element.style.display = "table-row-group";
+    } else {
+        element.style.display = "none";
+    }
+}
+
+var elements = document.getElementsByClassName("course-details");
+for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "none";
 }
